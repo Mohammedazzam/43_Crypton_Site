16c5d7ce9857d773a74a333f164e998af33a1bb9
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2023-02-21 00:22:54 +0200

    End NavBar

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger')
+const navUl = document.querySelector('.nav-ul')
+
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('active')
+    navUl.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-a').forEach(n=> n.addEventListener('click',function(){
+    hamburger.classList.remove('active');
+    navUl.classList.toggle('active')
+}))
